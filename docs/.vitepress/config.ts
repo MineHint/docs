132c67a825b5adda03a6dbf978c188b9a5841d7e
MinedPlumr <minedplumr11@gmail.com>
2025-07-04 15:31:31 +1000

    commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+// docs/.vitepress/config.ts
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    title: 'My Docs',
+    vite: {
+        server: {
+            port: 80
+        }
+    }
+})
