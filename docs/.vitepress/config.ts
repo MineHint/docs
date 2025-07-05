2076bb9ff57495101287783074a16d8a4e4e26be
MinedPlumr <111970002+MinedPlumr@users.noreply.github.com>
2025-07-06 01:44:05 +1000

    Delete docs directory

--- docs/.vitepress/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-export default defineConfig({
-    title: 'MineHint Docs',
-    description: 'MineHint Documentation',
-    base: '/',
-    outDir: './dist',
-    cleanUrls: true,
-})
