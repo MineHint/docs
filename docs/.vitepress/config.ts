226410391a60b2973e810c758bafa4b582f28ef5
MinedPlumr <111970002+MinedPlumr@users.noreply.github.com>
2025-07-06 01:36:47 +1000

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,11 +4,6 @@ export default defineConfig({
     title: 'MineHint Docs',
     description: 'MineHint Documentation',
     base: '/',
-    vite: {
-        server: {
-            port: 80
-        }
-    },
     outDir: './dist',
     cleanUrls: true,
 })
