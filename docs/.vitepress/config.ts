08d4e4920c83600a55a6f3221d73fcc5500c21b1
MinedPlumr <111970002+MinedPlumr@users.noreply.github.com>
2025-07-06 01:59:59 +1000

    Create config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    title: 'MineHint Docs',
+    description: 'MineHint Documentation',
+    base: '/',
+    outDir: './dist',
+    cleanUrls: true
+})
